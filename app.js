2889c4d0e03565b000278969fd10cf62dbd1c395
pischa <pischa@naver.com>
2021-07-01 17:22:26 +0900

    start

---
+++ app.js
@@ -0,0 +1,59 @@
+ // 1. mongoose 모듈 가져오기
+var mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://172.21.75.150:27017/admin', {
+    dbName: 'nodejs',
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+ 
+var db = mongoose.connection;
+// 4. 연결 실패
+db.on('error', function(){
+    console.log('Connection Failed!');
+});
+// 5. 연결 성공
+db.once('open', function() {
+    console.log('Connected!');
+});
+
+
+var users = mongoose.Schema({
+    name : 'string',
+    age : 'number', 
+    comment : 'string'
+});
+
+var Users = mongoose.model('users', users);
+
+//var newStudent = new Users({name:'Hong Gil Dong',  age:22, comment:'서울시 강남구 논현동'});
+
+/*newStudent.save(function(error, data){
+    if(error){
+        console.log(error); 
+    }else{
+        console.log('Saved!')
+    }
+});*/
+
+Users.find(function(error, users1){
+    console.log('--- Read all ---');
+    if(error){
+        console.log(error);
+    }else{
+        console.log(users1);
+    }
+})
+/*
+Users.remove({_id:'60d01af2a447c346a8d5dda0'}, function(error,output){
+    console.log('--- Delete ---');
+    if(error){
+        console.log(error);
+    }
+
+    console.log('--- deleted ---');
+});
+*/
